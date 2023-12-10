c5b11b745f08f04384560bf0162d5f86d169f185
zheniskyzy <madik_555@inbox.ru>
2023-12-11 03:45:58 +0600

    Second commit

--- Contents.swift
+++ Contents.swift
@@ -277,3 +277,361 @@ for i in array {
     }
 }
 print(count) // тоже самое
+
+//Lab 7
+// Задание 1
+
+struct Gps {
+    var latitute = 0.0
+    var longitude = 0.0
+}
+
+var somePlace = Gps()
+print(somePlace.latitute , somePlace.longitude)
+somePlace.latitute = 51.514004
+somePlace.longitude = 0.125226
+print(somePlace.latitute, somePlace.longitude)
+
+// Задание 2 и 10 (создание метода и его вызов)
+struct Book {
+    var title = ""
+    var author = ""
+    var pages = 0
+    var prices = 0.0
+    
+    func description(){
+        print("типа тут факты о книги")
+    }
+}
+var favoriteBook = Book()
+favoriteBook.author = "Харуки Мураками"
+favoriteBook.pages = 350
+favoriteBook.prices = 2550
+favoriteBook.title = "О чем я говорю, когда говорю о беге"
+print("Факты о моей любимой книги: \(favoriteBook.author) , название книги:  \(favoriteBook.title) , цена \(favoriteBook.prices) тенге, у нее  \(favoriteBook.pages) станиц") // название книги не смогла внести в ковычки(личная инициатива которая не получилась)
+
+favoriteBook.description() // вызов метода
+
+// Задание 3
+struct RunningWorkout{
+    var distance = 0.0
+    var time = 0.0
+    var elevation = 0.0
+}
+var firstRun = RunningWorkout()
+print("время занятий: \(firstRun.time), дистанция: \(firstRun.distance) и высота подьема: \(firstRun.elevation)")
+firstRun.distance = 2396
+firstRun.elevation = 94
+firstRun.time = 15.3
+print("время занятий: \(firstRun.time), дистанция: \(firstRun.distance) и высота подьема: \(firstRun.elevation)")
+
+// Задание 4
+struct Gps2 {
+    var latitute:Double
+    var longitude:Double
+}
+let someWhere = Gps2(latitute: 51.514004, longitude: 0.125226)
+print(someWhere.latitute, someWhere.longitude)
+
+// Задание 5
+struct Book2 {
+    var title: String
+    var author: String
+    var pages:Int
+    var prices:Double
+}
+var favoriteBook2 = Book2(title: "О чем я говорю, когда говорю о беге", author: "Харуки Мураками", pages: 350, prices: 2550)
+print("Факты о моей любимой книги: \(favoriteBook.author) , название книги:  \(favoriteBook.title) , цена \(favoriteBook.prices) тенге, у нее  \(favoriteBook.pages) станиц")
+
+// Задание 6
+struct Laptop{
+    var screenSize: Int = 13
+    var repairCount: Int = 0
+    var yearPurchased: Int
+}
+var macBook = Laptop(yearPurchased: 2021)
+var macAir = Laptop(yearPurchased: 2020)
+
+// Задание 7
+struct Height {
+    var heightInInches: Double
+    var heightInCentimeters: Double
+    
+    init(heightInInches: Double){
+        self.heightInInches = heightInInches
+        self.heightInCentimeters = heightInInches * 2.54
+    }
+    init(heightInCentimeters: Double){
+        self.heightInCentimeters = heightInCentimeters
+        self.heightInInches = heightInCentimeters / 2.54
+    }
+}
+var someonesHeight = Height(heightInInches: 65)
+print(someonesHeight.heightInCentimeters)
+var myHeight = Height(heightInCentimeters: 170)
+print("в дюймах \(myHeight.heightInInches) и в сантиметрах \(myHeight.heightInCentimeters)")
+
+// Задание 8
+struct User{
+    var name: String
+    var age: Int
+    var weight: Double
+    var height: Double
+    var activityLevel = 5
+}
+ var meAsUser = User(name: "Madina", age: 26, weight: 70, height: 170)
+print(meAsUser)
+
+// Задание 9
+struct Distance{
+    var meters: Double
+    var feet: Double
+    init(meters: Double){
+        self.meters = meters
+        self.feet = meters * 3.28084
+    }
+    init(feet: Double){
+        self.feet = feet
+        self.meters = feet / 3.28084
+    }
+}
+var mile = Distance(meters: 1600)
+print("в фунтах \(mile.feet) и в метрах \(mile.meters)")
+
+var myMile = Distance(feet: 8575)
+print("в фунтах \(myMile.feet) и в метрах \(myMile.meters)")
+
+// Задание 11
+struct Post {
+    var message: String
+    var likes: Int
+    var numberOfComments: Int
+    mutating func likesChange(){
+        likes += 1
+    }
+}
+var postE = Post(message: "hahah", likes: 3, numberOfComments: 10)
+print(postE.likes)
+postE.likesChange()
+print(postE.likes)
+
+
+// Задание 12
+struct RunningWorkout1 {
+    var distance: Double
+    var time: Double
+    var elevation: Double
+    func postWorkoutStats(){
+        print("Подробна инфа: время: \(time), дистанция \(distance) и высота \(elevation)")
+    }
+}
+var runningWorkout1 = RunningWorkout1(distance: 1000, time: 80, elevation: 60)
+runningWorkout1.postWorkoutStats()
+
+
+// Задание 13
+struct Steps {
+    var steps: Int
+    var goal: Int
+    mutating func takeStep(){
+      steps += 1
+    }
+}
+var stepsE = Steps(steps: 20, goal: 100)
+stepsE.takeStep()
+print(stepsE.steps)
+
+// Задание 14 не знаю правильно ли поняла задание
+struct Rectangle{
+    var width: Double
+    var height: Double
+    var area: Double {
+        width * height
+    }
+}
+var rectangle2 = Rectangle(width: 23, height: 10)
+print(rectangle2.area)
+
+// Задание 15
+struct Height1 {
+    var heightInInches: Double{
+        didSet{
+            var cm = heightInInches * 2.54
+            if cm != heightInCentimeters{
+                heightInCentimeters = cm
+            }
+        }
+    }
+    
+    var heightInCentimeters: Double{
+        didSet{
+            var inch = heightInCentimeters / 2.54
+            if inch != heightInInches{
+                heightInInches = inch
+            }
+        }
+    }
+    
+    init(heightInInches: Double) {
+        self.heightInInches = heightInInches
+        self.heightInCentimeters = heightInInches*2.54
+    }
+    
+    init(heightInCentimeters: Double) {
+        self.heightInCentimeters = heightInCentimeters
+        self.heightInInches = heightInCentimeters/2.54
+    }
+}
+
+var height1 = Height1(heightInInches: 200)
+print(height1.heightInInches, height1.heightInCentimeters)
+height1.heightInInches = 300
+print(height1.heightInInches, height1.heightInCentimeters)
+
+// Задание 16 не понимаю правильно ли я его решила :)
+struct RunningWorkout2 {
+    var distance: Double
+    var time: Double
+    var elevation: Double
+    var averageMileTime: Double{
+         (distance / 1600) / time
+    }
+    }
+var runningWorkout2 = RunningWorkout2(distance: 500, time: 60, elevation: 70)
+print(runningWorkout2.averageMileTime)
+
+struct Steps3 {
+    var steps: Int{
+        willSet{
+            var stepsPlusOne = steps + 1
+            if stepsPlusOne == goal{
+                print("поздравляю оно равно!")
+            }
+        }
+    }
+    var goal: Int
+    
+    mutating func takeStep() {
+        steps += 1
+    }
+}
+var steps3 = Steps3(steps: 9999, goal: 10000)
+steps3.takeStep()
+
+// Задание 17
+// что-то сделала, что-то получилось, непонятно что
+
+struct User2 {
+    var userName: String
+    var email: String
+    var age: Int
+    static var currentUser: User2 = User2(userName: "Madina", email: "madik_555@inbox.ru ", age: 26)
+    
+    func logIn(user2: User2){
+        User2.currentUser = user2
+        print( "\(User2.currentUser.userName) вошел в систему.")
+    }
+}
+//создала два экземпляра, у одного вызываю метод и передаюему другой экземпляр
+// и кажется что-то тут не так
+var anotherUser = User2(userName: "Serper", email: "serper@gmail.com", age: 26)
+var anotherUser2 = User2(userName: "Sultan", email: "sultik_456@gmail.com", age: 20)
+anotherUser.logIn(user2: anotherUser2)
+
+
+//Lab 8 Задание 1
+class Spaceship {
+    var name = ""
+    var health = 0
+    var position = 0
+    func moveLeft(num: Int){
+        if num < position{
+            position -= 1
+        }
+    }
+    func moveRight(num: Int){
+        if num > position{
+            position += 1
+        }
+    }
+    func wasHit(){
+        health -= 5
+        if health <= 0{
+            print("Извините, ваш корабль был сбит слишком много раз. Хотите сыграть еще раз?")
+        }
+    }
+}
+let falcon = Spaceship()
+falcon.name = "Falcon"
+
+falcon.moveLeft(num: -10) // а если сюда написать положительное число, ничего не произойдет, правильно ли это?
+print(falcon.position)
+falcon.moveLeft(num: -4)
+print(falcon.position)
+falcon.moveRight(num: 9)
+print(falcon.position)
+
+falcon.wasHit()
+print(falcon.health)
+
+
+// Задание 2
+class Fighter: Spaceship {
+    var weapon = ""
+    var remainingFirePower = 5
+    func fire(){
+        if remainingFirePower > 0{
+            remainingFirePower -= 1
+        }else{
+            print("У вас больше нет оружейной мощности")
+        }
+    }
+}
+
+var destroyer = Fighter()
+destroyer.weapon = "лазер"
+destroyer.remainingFirePower = 10
+destroyer.name = "Destroyer"
+print(destroyer.position)
+destroyer.moveRight(num: 5)
+print(destroyer.position)
+/* Ответ почему 'weapon' на falcon не работает, потому что falcon экземпляр класса Spaceship.
+ А weapon это свойство Fighter, т.е. Что бы иметь доступ к weapon, falcon должен
+ наследоваться от Fighter. Вроде верно? :)
+*/
+destroyer.fire()
+print(destroyer.remainingFirePower)
+destroyer.fire()
+print(destroyer.remainingFirePower)
+destroyer.fire()
+print(destroyer.remainingFirePower)
+destroyer.fire()
+print(destroyer.remainingFirePower)
+
+
+// Задание 3
+class ShieldedShip: Fighter {
+    var shieldStrength = 25
+    override func wasHit() {
+        if shieldStrength > 0{
+            shieldStrength -= 5
+        }else{
+           super.wasHit()
+        }
+    }
+}
+var defender = ShieldedShip()
+defender.name = "Defender"
+defender.weapon = "Cannon"
+defender.moveRight(num: 50)
+print(defender.position)
+defender.fire()
+print(defender.remainingFirePower)
+defender.wasHit()
+print("значение shieldStrength = \(defender.shieldStrength) и значение здоровья = \(defender.health)")
+
+// Задание 4
+/* не стала его делать и менять все в коде выше ибо
+ ответы(решения) уже предоставлены в самой задаче
+ Вроде ничего не упустила)))
+ */
